caa7f20743a0de1ad32b1343b45db91e961a208e
Callum Rogers <callumrogers@hotmail.co.uk>
2013-07-15 22:19:58 +0100

    Made a non-working parser in FParsec

--- RaskellCompiler/Program.fs
+++ RaskellCompiler/Program.fs
@@ -1,7 +1,14 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open FParsec.CharParsers
+open Parser
+
+let test p str =
+    match run p str with
+        | Success (result, _, _)  -> printfn "Success: %A" result
+        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
+    test topLevelDefn "abc b c = (d e) f"
+
     printfn "%A" argv
     0 // return an integer exit code
---
+++ RaskellCompiler/Tree.fs
@@ -0,0 +1,14 @@
+﻿module Tree
+
+type NameRec = { Name: string }
+type AppRec = { F: Expr; Args: Expr list }
+
+and Expr = 
+    | Ident of NameRec
+    | Type of NameRec
+    | App of AppRec
+
+type FuncDefRec = { Name: string; Params: string list; Expr: Expr }
+
+type TopDecl =
+    | FuncDef of FuncDefRec
